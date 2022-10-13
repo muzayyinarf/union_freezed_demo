630d1c5d8abc2b60597d00ada4725011391b993d
Muzayyin Arifin <muzayyinarifin46@gmail.com>
2022-10-13 10:32:27 +0700

    initial source

---
+++ android/app/src/main/kotlin/com/muzayyinarf/union_freezed_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.muzayyinarf.union_freezed_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import '/number_repository.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int? number;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Union using Freezed'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '$number',
+            style: const TextStyle(fontSize: 40),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  number = NumberRepository().getNumber();
+                });
+              },
+              child: const Text('Get Data'))
+        ],
+      )),
+    );
+  }
+}
